cd5b55c9d5962bcc878f9576d06e7b2c15da2df8
Stefano Alloro <steve.alloro@gmail.com>
2014-01-15 08:53:15 +0100

    minor update to internal deployment scripts;

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -8,4 +8,10 @@ module Version
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD}"
+
+  # Compact version of the versioning costant.
+  COMPACT = "#{MAJOR.gsub('.','')}#{MINOR}"
+
+  # Current internal DB version (indipendent from migrations and framework release)
+#  DB      = "3.05.07"
 end
