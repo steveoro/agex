9dbd2680451cd89e74f03db7f6f665605747f4ed
steveoro <steve.alloro@gmail.com>
2015-05-28 13:39:05 +0200

    updated versioning

--- lib/framework/version.rb
+++ lib/framework/version.rb
@@ -1,9 +1,9 @@
 module Version
   # [Steve, 20080414]
   # ** DO NOT CHANGE THE FOLLOWING UNLESS YOU KNOW WHAT YOU'RE DOING!! **
-  MAJOR   = '3.05'
-  MINOR   = '07'
-  BUILD   = '20140114'
+  MAJOR   = '3.06'
+  MINOR   = '00'
+  BUILD   = '20150528'
 
   # Internal constant used to discriminate between all the existing and
   # running versions of the AgeX framework.
